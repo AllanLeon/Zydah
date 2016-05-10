57775df5aabd21e4a2e087274891ba60f13ba8cd
Franco Montiel <francomontiel@hotmail.com>
2016-05-09 18:48:25 -0700

    Spinner at videoplayer controller now working

--- www/scripts/videoplayer.js
+++ www/scripts/videoplayer.js
@@ -1,14 +1,16 @@
 app.controller('videoplayerController', function($scope){
 	$scope.demoVideoURL = '../assets/videos/love.mp4';
 	$scope.rating = 0;
+	$scope.plus1 = plus1;
+	$scope.minus1 = minus1;
 
 	function plus1() {
-		console.log('ASS');
-		//$scope.rating = 1;
+		$scope.rating++;
 	}
 
 	function minus1() {
-		console.log('ASS');
-		//$scope.rating = 1;
+		if ($scope.rating != 0) {
+			$scope.rating--;
+		}
 	}
 });
