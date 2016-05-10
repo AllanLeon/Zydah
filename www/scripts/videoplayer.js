3d3eb5692bb17ea302c1ca4cf0cb317b35d5466c
Allan Leon <aleon.107@outlook.com>
2016-05-09 20:19:40 -0400

    Implemented basic angular module on videoplayer

--- www/scripts/videoplayer.js
+++ www/scripts/videoplayer.js
@@ -1,16 +1,55 @@
 app.controller('videoplayerController', function($scope){
-	$scope.demoVideoURL = '../assets/videos/love.mp4';
-	$scope.rating = 0;
+	$scope.video = {
+		'title': 'A Love Story.',
+		'description': 'Falcon Fan realized broke up with his girlfriend Fedora Lover.',
+		'tags': ['comedy', 'romance'],
+		'rating': 9001,
+		'url': '../assets/videos/love.mp4'
+	};
+
+	$scope.video.comments = [
+		{
+			'name': 'A',
+			'img': '../assets/images/user_circle.png',
+			'text': 'first comment'
+		},
+		{
+			'name': 'b',
+			'img': '../assets/images/user_circle.png',
+			'text': 'wqert'
+		},
+		{
+			'name': 'wqertyujh',
+			'img': '../assets/images/user_circle.png',
+			'text': 'qwertghjhgvcxz'
+		},
+		{
+			'name': 'qqwq',
+			'img': '../assets/images/user_circle.png',
+			'text': 'rtyuiolkmnbv'
+		},
+		{
+			'name': 'poqe',
+			'img': '../assets/images/user_circle.png',
+			'text': 'wqerghfdsa'
+		},
+		{
+			'name': 'qwertyn',
+			'img': '../assets/images/user_circle.png',
+			'text': 'qwertglklm'
+		}
+	];
+
 	$scope.plus1 = plus1;
 	$scope.minus1 = minus1;
 
 	function plus1() {
-		$scope.rating++;
+		$scope.video.rating++;
 	}
 
 	function minus1() {
 		if ($scope.rating != 0) {
-			$scope.rating--;
+			$scope.video.rating--;
 		}
 	}
 });
