8ea2c0c5860dd8b770b311bffb1c158e62bc0a46
Allan Leon <aleon.107@outlook.com>
2016-05-11 01:17:02 -0400

    Implemented beta routing

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('zydah', []);
+var app = angular.module('zydah', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		'templateUrl': 'views/main.html',
+		'controller': 'mainController'
+	})
+	.when('/search/:query', {
+		'templateUrl': 'views/search.html',
+		'controller': 'searchController'
+	})
+	.when('/video/:id', {
+		'templateUrl': 'views/video_player.html',
+		'controller': 'videoplayerController'
+	})
+});
