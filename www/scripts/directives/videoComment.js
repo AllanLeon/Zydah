eca09db48bfcb7491fca8835fb7aaf5b957d3f81
Allan Leon <aleon.107@outlook.com>
2016-05-23 03:52:01 -0400

    Created videoComment directive

---
+++ www/scripts/directives/videoComment.js
@@ -0,0 +1,22 @@
+app.directive('videoComment', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			commentData: "="
+		},
+		template:
+		'<article>' +
+			'<div class="comment">' +
+				'<div class="usr_img">' +
+					'<figure>' +
+						'<img ng-src="{{ commentData.img }}" alt="{{ commentData.name }}"/>' +
+					'</figure>' +
+				'</div>' +
+				'<div class="usr_text">' +
+					'<h3>{{ commentData.name }}</h3>' +
+					'<span>{{ commentData.text }}</span><br>' +
+				'</div>' +
+			'</div>' +
+		'</article>'
+	}
+});
